22258a7295c7b017ea3a86a55c92435271ed24a5
prasanna <prasanna709397300@gmail.com>
2023-04-19 10:34:34 +0530

    asss

---
+++ assign1.py
@@ -0,0 +1,17 @@
+marks=int(input('Enter marks out of 100'))
+if(marks >90 and marks<=100):
+  print('grade is A+')
+elif (marks>80 and marks<=90):
+  print('grade is A')
+elif (marks>70 and marks<=80):
+  print('grade is B+')
+elif(marks>60 and marks<=70):
+  print('grade is B')
+elif(marks>50 and marks<=60):
+  print('grade is C+')
+elif(marks>40 and marks<=50):
+  print('grade is C')
+else:
+  print('grade is D')
+
+
